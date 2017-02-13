fddcc9413704c6f813953fdb5d0c8ca36e06b196
Matthew Perry <perrygeo@gmail.com>
2017-02-12 17:55:53 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ try:
 except ImportError:
     from distutils.core import setup
 
-LONG_DESCRIPTION = """`simanneal` is a python implementation of the 
-[simulated annealing optimization](http://en.wikipedia.org/wiki/Simulated_annealing) technique. 
+LONG_DESCRIPTION = """`simanneal` is a python implementation of the
+[simulated annealing optimization](http://en.wikipedia.org/wiki/Simulated_annealing) technique.
 
-Simulated annealing is used to find a close-to-optimal solution among an 
-extremely large (but finite) set of potential solutions. It is particularly 
-useful for [combinatorial optimization](http://en.wikipedia.org/wiki/Combinatorial_optimization) 
-problems defined by complex objective functions that rely on external data. 
+Simulated annealing is used to find a close-to-optimal solution among an
+extremely large (but finite) set of potential solutions. It is particularly
+useful for [combinatorial optimization](http://en.wikipedia.org/wiki/Combinatorial_optimization)
+problems defined by complex objective functions that rely on external data.
 """
 
 setup(name='simanneal',
-      version='0.1.2',
+      version='0.2.0',
       description='Simulated Annealing in Python',
       license='BSD',
       author='Matthew Perry',
